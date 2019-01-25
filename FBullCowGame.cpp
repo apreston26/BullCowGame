2cbb37011cdb2ff80840e08b120929718cd0bbbe
apreston26 <47041524+apreston26@users.noreply.github.com>
2019-01-25 14:34:43 -0700

    Bull Cow Game
    This is an earlier version, need to go back and add limit on guessing and solution verification.

---
+++ FBullCowGame.cpp
@@ -0,0 +1,63 @@
+#include "FBullCowGame.h"
+
+using int32 = int; 
+
+FBullCowGame::FBullCowGame() { Reset(); }
+
+int32 FBullCowGame::GetMaxTries() const { return MyMaxTries;}
+int32 FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
+int32 FBullCowGame::GetHiddenWordLength() const { return MyHiddenWord.length(); }
+void FBullCowGame::Reset()
+{
+	constexpr int32 MAX_TRIES = 8;
+	const FString HIDDEN_WORD = "ant";
+
+	MyMaxTries = MAX_TRIES;
+	MyHiddenWord = HIDDEN_WORD;
+	MyCurrentTry = 1;
+	return;
+}
+
+bool FBullCowGame::IsGameWon() const
+{
+	return false;
+}
+
+bool FBullCowGame::CheckGuessValidity(FString) const
+{
+	return false;
+}
+
+//receives a VALID guess, increments turn, and returns count
+FBullCowCount FBullCowGame::SubmitGuess(FString Guess)
+{
+	//increment the turn number 
+	MyCurrentTry++;
+
+	//setup a return vairable 
+	FBullCowCount BullCowCount;
+
+	//loop through all letters in the guess 
+	int32 HiddenWordLength = MyHiddenWord.length();
+	for (int32 MHWChar = 0; MHWChar < HiddenWordLength; MHWChar++) 
+	{
+		//compare letters against the hidden word 
+		for (int32 GChar = 0; GChar < HiddenWordLength; GChar++)
+		{
+			// if they match then 
+			if (Guess[GChar] == MyHiddenWord[MHWChar])
+			{
+			//if they're in the same place 
+				if (MHWChar == GChar)
+				{
+					BullCowCount.Bulls++; //increment bulls 
+				}
+				else 
+				{
+					BullCowCount.Cows++; //if not a bull it must be a cow 
+				}
+			}
+		}
+	}
+	return BullCowCount;
+}
---
+++ main.cpp
@@ -0,0 +1,85 @@
+/* This is the console executablem that makes us of the BUllCow class 
+This acts as the view in a MVC pattern, and is responsible for all
+user interaction. For game logic the FBullCowGame class. 
+*/
+
+#include <iostream>
+#include <string>	
+#include "FBullCowGame.h"
+
+using FText = std::string;
+using int32 = int; 
+
+void PrintIntro();
+void PlayGame();
+FText GetGuess();
+bool AskToPlayAgain();
+
+FBullCowGame BCGame; // instantiate a new game
+
+// the entry point32 for our application
+int main()
+{
+	bool bPlayAgain = false;
+	do {
+		PrintIntro();
+		PlayGame();
+		bPlayAgain = AskToPlayAgain();
+	}
+	while (bPlayAgain);
+
+	return 0; // exit the application
+}
+
+ 
+// introduce the game
+void PrintIntro()
+{
+	std::cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	std::cout << "Can you guess the " << BCGame.GetHiddenWordLength();
+	std::cout << " letter isogram I'm thinking of?\n";
+	std::cout << std::endl;
+	return;
+}
+
+
+void PlayGame()
+{
+	BCGame.Reset();
+	int32 MaxTries = BCGame.GetMaxTries();
+	
+	// loop for the number of turns asking for guesses
+	// TODO change from FOR to WHILE loop once we are validating tries
+	for (int32 count = 1; count <= MaxTries; count++) {
+		FText Guess = GetGuess(); // TODO make loop checking valid
+		
+		// submit valid guess to the game, and receive counts
+		FBullCowCount BullCowCount = BCGame.SubmitGuess(Guess);
+		// print32 number of bulls and cows
+		std::cout << "Bulls = " << BullCowCount.Bulls;
+		std::cout << ". Cows = " << BullCowCount.Cows << std::endl;
+		std::cout << std::endl;
+	}
+
+	// TODO summarise game
+}
+
+
+FText GetGuess()
+{
+	int32 CurrentTry = BCGame.GetCurrentTry();
+
+	// get a guess from the player
+	std::cout << "Try " << CurrentTry << ". Enter your guess: ";
+	FText Guess = "";
+	std::getline(std::cin, Guess); 
+	return Guess;
+}
+
+bool AskToPlayAgain()
+{
+	std::cout << "Do you want to play again (y/n)? ";
+	FText Response = "";
+	std::getline(std::cin, Response);
+	return (Response[0] == 'y') || (Response[0] == 'Y');
+}
